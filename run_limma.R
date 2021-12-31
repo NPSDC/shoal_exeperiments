e42a7eeb602a292f9a41822f89a4f7ec7f918f87
NPSDC <noorpratap@gmail.com>
2021-12-31 09:54:10 -0500

    limma code added

---
+++ run_limma.R
@@ -0,0 +1,135 @@
+suppressPackageStartupMessages(library(tximeta))
+suppressPackageStartupMessages(library(tximport))
+suppressPackageStartupMessages(library(limma))
+suppressPackageStartupMessages(library(edgeR))
+suppressPackageStartupMessages(library(fishpond))
+suppressPackageStartupMessages(library(iCOBRA))
+suppressPackageStartupMessages(library(DESeq2))
+suppressPackageStartupMessages(library(SummarizedExperiment))
+
+edgerPrep <- function(cts, condition = NULL, cov = NULL) {
+    y <- DGEList(cts)
+    y <- y[filterByExpr(y),]
+    y <- calcNormFactors(y)
+    if (is.null(cov)){
+        design <- model.matrix(~condition)
+    } else {
+        design <- model.matrix(~condition + cov)
+    }
+    list(y, design)
+}
+
+limmavoom <- function(cts, condition = NULL, cov = NULL) {
+    out <- edgerPrep(cts, condition = condition, cov = cov)
+    y <- out[[1]]; design <- out[[2]]
+    v <- voom(y, design)
+    fit <- lmFit(v, design)
+    fit <- eBayes(fit)
+    topTable(fit, coef=2, n=nrow(y), sort="none")
+}
+
+deseq2 <- function(cts, condition = NULL, cov = NULL) {
+    cts <- round(cts)
+    suppressMessages({
+        if (is.null(cov)) {
+            samps <- data.frame(condition=condition)
+            dds <- DESeqDataSetFromMatrix(cts, samps, ~condition)
+        } else {
+            samps <- data.frame(condition=condition, cov=cov)
+            dds <- DESeqDataSetFromMatrix(cts, samps, ~condition + cov)
+        }
+        dds <- DESeq(dds, minReplicates=Inf, quiet=TRUE)
+    })
+    res <- results(dds, name="condition_2_vs_1")
+    res$padj[is.na(res$padj)] <- 1
+    res
+}
+
+myicobra <- function(cd, lvl="Gene", n.sub) {
+    cp <- calculate_performance(cd,
+                                binary_truth="status",
+                                aspect=c("fdrtpr","fdrtprcurve"),
+                                thrs=c(.01,.05,.1)
+                                )
+    cobraplot <- prepare_data_for_plot(cp,
+                                       #colorscheme=cols,
+                                       facetted = TRUE,
+                                       incloverall = TRUE)
+    yrng <- c(.4, 1)
+    xrng <- c(0,max(.2, max(fdrtpr(cp)$FDR)))
+    plot_fdrtprcurve(cobraplot, plottype="points",
+                     pointsize=2.5,
+                     xaxisrange=xrng,
+                     yaxisrange=yrng,
+                     title=paste0(lvl,"-level, n=",n.sub," vs ",n.sub))
+}
+
+getQuantFiles <- function(dir, type="shoal") {
+    samples <- paste(outer(c(1:6), c(1:2), function(x,y) paste(x,y,sep="_")))
+    if(!type %in% c("shoal", "salmon"))
+        stop(paste(type, "is, expected shoal or salmon"))
+    if(type == "salmon")
+        files <- file.path(dir, samples, "quant.sf")
+    if(type == "shoal")
+        files <- file.path(dir, samples, "adapt_quant.sf")
+    
+    colData <- data.frame(files = files, names = samples, condition = as.factor(rep(c(1,2),each=6)))
+    return(colData)
+}
+
+getCounts <- function(dfSalFiles, dfShoalFiles) {
+    se <- tximeta::tximeta(dfSalFiles)
+    txi <- tximport(dfSalFiles[["files"]], type="salmon", txOut=T,
+                    countsFromAbundance="lengthScaledTPM")
+    y <- labelKeep(se)
+    y <- y[mcols(y)[["keep"]],] 
+    
+    seShoal <- se
+    txiShoal <- tximport(dfShoalFiles[["files"]], type="salmon", txOut=T,
+                    countsFromAbundance="lengthScaledTPM")
+    lShoal <- lapply(dfShoalFiles[["files"]], function(f) read.delim(f,row.names = 1))
+    cShoal <- matrix(0, dim(lShoal[[1]])[1], length(lShoal), dimnames = list(rownames(lShoal[[1]]),
+            dfShoalFiles[["names"]]))
+    for(i in seq_along(lShoal))
+        cShoal[,i] <- lShoal[[i]][["NumReads"]]
+    assays(seShoal)[["counts"]] <- cShoal
+    yShoal <- labelKeep(seShoal)
+    yShoal <- yShoal[mcols(yShoal)[["keep"]],] 
+    return(list(txiSal = txi, ySal = y, txiShoal = txiShoal, yShoal = yShoal))
+}
+
+runlimma <- function(countList) {
+    allTxps <- union(rownames(countList[["ySal"]]), rownames(countList[["yShoal"]]))
+    dSal <- limmavoom(countList[["txiSal"]][["counts"]][allTxps,], countList[["ySal"]][["condition"]])
+    dShoal <- limmavoom(countList[["txiShoal"]][["counts"]][allTxps,], countList[["yShoal"]][["condition"]])
+    
+    unTxps <- union(rownames(dSal), rownames(dShoal))
+    missSal <- setdiff(unTxps, rownames(dSal))
+    dSalU <- rbind(dSal, matrix(1, nrow = length(missSal), ncol = ncol(dSal),
+                                dimnames = list(missSal, colnames(dSal))))
+    missShoal <- setdiff(unTxps, rownames(dShoal))
+    dShoalU <- rbind(dShoal, matrix(1, nrow = length(missShoal), ncol = ncol(dShoal),
+                                    dimnames = list(missShoal, colnames(dShoal))))
+    dShoalU <- dShoalU[unTxps,]
+    dSalU <- dSalU[unTxps,]
+    
+    padj <- data.frame(row.names = rownames(dSalU))
+    iso.any <- iso.dtu | iso.dte | iso.dge
+    status <- as.numeric(rownames(padj) %in% names(iso.dtu)[iso.any])
+    truth <- data.frame(status, row.names=rownames(padj))
+    
+    padj[["Sal"]] <- dSalU[["adj.P.Val"]]
+    padj[["Shoal"]] <- dShoalU[["adj.P.Val"]]
+    
+    cd <- COBRAData(padj=padj, truth=truth)
+    return(myicobra(cd, "Transcript", 6))
+}
+
+runDE <- function(dirSal, dirShoal) {
+    dfSal <- getQuantFiles(dirSal, type="salmon")
+    dfShoal <- getQuantFiles(dirShoal, type="shoal")
+    
+    countsList <- getCounts(dfSal, dfShoal)
+    cob <- runlimma(countsList)
+    return(cob)
+}
